ca856dd5a975a826ab1e8c8ea08e6183a10576e2
jmoyacurbelo <jmoyacurbelo@gmail.com>
2019-12-30 11:44:57 -0600

    Opreaciones con conjuntos (sets)

--- Sets.py
+++ Sets.py
@@ -10,6 +10,42 @@ X & Y # Interseccion de conjuntos
 X | Y # union
 X-Y # diferencia
 
+X > Y, X < Y # Superset, subset
+
+X.add('JOSE')
+
+X.remove('m')
+
+for item in set('abc'):
+    print(item*3)
+
+# Inicialaizar un conjunto vacío
+
+S = set()
+
+S.add('Jose')
+
+# Nota
+# Los conjuntos solo pueden tener objetos inmutables dentro, por ello ni las listas,
+# ni los diccionarios pueden estar dentro de los conjuntos pero si las tuplas
+
+############################################
 # Comprehension in Sets
+############################################
 
 {x**2 for x in range(1,5)}
+
+{x for x in 'spam'}
+
+L = [1,2,1,3,2,4,5]
+
+L = list(set(L))  # Remover duplicados
+
+# operaciones mas realistas
+
+engineers = {'bob', 'sue', 'ann', 'vic'}
+managers = {'tom', 'sue'}
+
+'bob' in engineers
+
+engineers & managers
