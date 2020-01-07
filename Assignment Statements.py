32d1b348de81b846664f85622182335f98686430
jmoyacurbelo <jmoyacurbelo@gmail.com>
2020-01-07 17:40:49 -0600

    Assignment Statements

---
+++ Assignment Statements.py
@@ -0,0 +1,53 @@
+import sys
+sys.modules[__name__].__dict__.clear()
+
+spam = 'Spam'   # Asignacion comun
+
+spam, ham = 'yum','YUM'
+
+[spam, ham] = ['yum','YUM']
+
+a,b,c,d = 'spam'    # Asignación sequencial a=s, b=p ....
+
+a, *b = 'spam'   # extended sequence unpacking a='s', b=['p','a','m']
+
+W = S = 'spam'
+
+spam = 0
+spam += 42   # equivalente a spam = spam + 42
+
+[a,b,c] = (1,2,3)  # Asigna los numeros
+
+(a,b,c) = 'ABC' # Asigna cada letra a la mayuscula
+
+red, blue, green = range(3)
+
+
+
+seq = [1,2,3,4,5]
+
+a, *b = seq  # primera forma
+
+*a, b = seq  # Segunda forma
+
+a, *b, c = seq  # Tercera forma
+
+# Ejemlo
+L = [1,2,3,4]
+while L:
+    a, L = L[0], L[1:]
+    print(a, L)
+
+# Ejemplo simplificado
+L = [1,2,3,4]
+while L:
+    a, *L = L
+    print(a, L)
+
+# Nota: *a simpre asignara una lista, sino hay nada que apilar asignará una lista vacía
+# y no se puede utilizar más de un apilamiento en la mismsa asignación
+
+L = [1,2,3]
+a, b, c, *d = L
+
+
