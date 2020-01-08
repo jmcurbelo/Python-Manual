6a01ec73f645d1685dc004f9ecfd4411e49ee603
jmoyacurbelo <jmoyacurbelo@gmail.com>
2020-01-08 16:53:54 -0600

    if Statement

---
+++ if Statement.py
@@ -0,0 +1,24 @@
+# general form
+# if <test1>:
+     #statements1
+# elif <test2>:
+     #statements2
+# else:
+     #statements3
+
+
+if X:
+    A = Y
+else:
+    A = Z
+
+# Esto es equivalente a escribir
+
+A = Y if X else Z
+
+# Ejemplo
+
+a = 't' if 'spam' else 'f'
+
+b = 't' if '' else 'f'
+
